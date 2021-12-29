8047942b39bf2f901b9bbeabbfecdf31d11aa6ca
maidul.bappy <maidul.bappy.001@gmail.com>
2021-12-29 08:43:41 +0600

    balance component update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Balance from "./components/Balance";
 import Header from "./components/Header";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
-import GlobalProvider from "./context/GlobalState";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
